7ee66a2d46c748fd35185735d435ca2c869c08b0
CloudSurferCode <35977211+CloudSurferCode@users.noreply.github.com>
2018-07-15 13:48:04 +0300

    Update vk_public_parser.py

--- vk_public_parser.py
+++ vk_public_parser.py
@@ -3,15 +3,14 @@ import pandas as pd
 import time
 import json
 
-vk_session = vk_api.VkApi('89263185183', 'mariamanush123')  # логин и пароль
+vk_session = vk_api.VkApi('your_login', 'your_password')  # login and password
 vk_session.auth()
 vk = vk_session.get_api()
 
 
 def main():
-    #    x = vk.friends.get(fields='contacts')
-
-    count_in = vk.groups.getMembers(group_id='thisis_media')
+    
+    count_in = vk.groups.getMembers(group_id='id_your_public')
     count = count_in['count']
     print(count)
     offset = 0
@@ -21,16 +20,13 @@ def main():
     data = []
 
     for count in range(count_in['count'], 0, -step):
-        y = vk.groups.getMembers(group_id='thisis_media', offset=offset,
+        y = vk.groups.getMembers(group_id='id_your_public', offset=offset,
                                  fields='contacts, city')
-        # data += y['items']
         data.extend(y['items'])
         time.sleep(3)
 
     df = pd.io.json.json_normalize(data)
     df.to_excel(r'thisis_media.xlsx', index=False)
-
-#https://conversiontools.io/conversion/convert_csv_to_excel #encoding
 
 if __name__ == '__main__':
     main()
