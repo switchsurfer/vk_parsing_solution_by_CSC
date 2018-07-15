7fa7d72cc03b939c6eb9576842bc91cbfc34523c
CloudSurferCode <35977211+CloudSurferCode@users.noreply.github.com>
2018-07-15 13:54:28 +0300

    Update flter.py

--- flter.py
+++ flter.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # read Excel in Pandas DataFrame
-df = pd.read_excel(r'thisis_media.xlsx', dtype={'mobile_phone':'str'})
+df = pd.read_excel(r'your_file.xlsx', dtype={'mobile_phone':'str'})
 
 # delete all except numbers
 df['mob_clean'] = df.mobile_phone.str.replace(r'\D','')
@@ -14,4 +14,4 @@ mask = (df['mob_clean'].str.len() >= 10) & (df['mob_clean'].str.contains(r'^[78]
 df.loc[~mask, ['mob_clean']] = ''
 
 # save DataFrame to Excel file
-df.to_excel(r'thisis_media_true.xlsx', index=False)
+df.to_excel(r'result.xlsx', index=False)
